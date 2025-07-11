f22951d40e59fbce46e52d928a6a2a3ead87a9cb
armstrong-codes <a.hirwa2@alustudent.com>
2025-07-11 21:08:43 +0000

    done

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -12,23 +12,23 @@ import sys
 def export_to_json(user_id):
     """Export user TODO list to JSON format"""
     base_url = 'https://jsonplaceholder.typicode.com'
-    
+
     # Get user information
     user_resp = requests.get('{}/users/{}'.format(base_url, user_id))
     if user_resp.status_code != 200:
         return
-    
+
     user = user_resp.json()
     username = user.get('username')
-    
+
     # Get user's TODO list
     todos_resp = requests.get('{}/todos'.format(base_url),
-                             params={'userId': user_id})
+                              params={'userId': user_id})
     if todos_resp.status_code != 200:
         return
-    
+
     todos = todos_resp.json()
-    
+
     # Prepare data in required JSON format
     data = {
         str(user_id): [
@@ -40,21 +40,21 @@ def export_to_json(user_id):
             for task in todos
         ]
     }
-    
+
     # Write to JSON file
     json_file = '{}.json'.format(user_id)
     with open(json_file, mode='w', encoding='utf-8') as f:
         json.dump(data, f)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
-    
+
     user_id_arg = sys.argv[1]
-    
+
     # Validate that user_id is a number
     if not user_id_arg.isdigit():
         sys.exit(1)
-    
+
     export_to_json(user_id_arg)
