a30a13936f59e4dd438df7a81d0708b7653de572
armstrong-codes <a.hirwa2@alustudent.com>
2025-07-12 18:33:00 +0000

    done

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,54 @@
 #!/usr/bin/python3
 """
-Exports all employees' TODO list progress to JSON format.
-Usage:
-    ./3-dictionary_of_list_of_dictionaries.py
+Exports to-do list data of all employees to a JSON file.
 """
+
 import json
 import requests
 
 
-def export_all_to_json():
-    """Export all employees' TODO list to JSON format"""
-    base_url = 'https://jsonplaceholder.typicode.com'
-    
-    # Get all users
-    users_resp = requests.get('{}/users'.format(base_url))
-    if users_resp.status_code != 200:
-        return
+def export_all_tasks_to_json():
+    """
+    Fetches all users and tasks and exports them to todo_all_employees.json
+    in the format:
+    {
+        "USER_ID": [
+            {
+                "username": "USERNAME",
+                "task": "TASK_TITLE",
+                "completed": TASK_COMPLETED_STATUS
+            },
+            ...
+        ],
+        ...
+    }
+    """
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    users = users_resp.json()
+    users = requests.get(users_url).json()
+    todos = requests.get(todos_url).json()
 
-    # Get all todos
-    todos_resp = requests.get('{}/todos'.format(base_url))
-    if todos_resp.status_code != 200:
-        return
-
-    todos = todos_resp.json()
-
-    # Map users by ID for quick access to username
-    user_dict = {user.get('id'): user.get('username') for user in users}
-
-    # Build the required dictionary structure
     all_tasks = {}
-    for task in todos:
-        user_id = task.get('userId')
-        username = user_dict.get(user_id)
-        
-        task_entry = {
-            "username": username,
-            "task": task.get('title'),
-            "completed": task.get('completed')
-        }
-        
-        if str(user_id) not in all_tasks:
-            all_tasks[str(user_id)] = []
-        all_tasks[str(user_id)].append(task_entry)
 
-    # Write to JSON file
-    with open('todo_all_employees.json', mode='w', encoding='utf-8') as f:
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+
+        user_tasks = [
+            {
+                "username": username,
+                "task": task.get('title'),
+                "completed": task.get('completed')
+            }
+            for task in todos if task.get('userId') == user_id
+        ]
+
+        all_tasks[str(user_id)] = user_tasks
+
+    with open("todo_all_employees.json", "w") as f:
         json.dump(all_tasks, f)
 
 
-if __name__ == '__main__':
-    export_all_to_json()
-
+if __name__ == "__main__":
+    export_all_tasks_to_json()
